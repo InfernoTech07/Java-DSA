b811f0d02c8f25df569926ec45136682988e98b6
Abhishek Kumar <109984370+InfernoTech07@users.noreply.github.com>
2023-09-18 22:07:33 +0530

    Add files via upload

---
+++ InputFromUser.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InputFromUser{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter value:");
+        int n=sc.nextInt();
+        System.out.println(n);
+
+        for(int i=0;i<=n;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
